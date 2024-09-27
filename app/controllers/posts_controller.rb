a541970134bd1e2bb7ee02aa40e3f8d3082da924
kodacamp <kodacamp.recruitment@gmail.com>
2024-09-27 11:03:45 +0800

    refactor: post_controller use callbacks

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,8 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+  # before_action :set_post, except: [:index, :new, :create]
+  # Todo: Try to learn about callbacks
+  # https://www.youtube.com/watch?v=SnRq1_VXVVc
 
   def index
     @posts = Post.all
@@ -18,32 +22,36 @@ class PostsController < ApplicationController
     # @post = Post.new(title: params[:post][:title],
     #                  content: params[:post][:content])
 
-    @post = Post.new(params[:post].permit(:title, :content))
+    @post = Post.new(post_params)
 
     if @post.save
       redirect_to posts_path
     end
   end
 
-  def show
-    @post = Post.find(params[:id])
-  end
+  def show; end
 
-  def edit
-    @post = Post.find(params[:id])
-  end
+  def edit; end
 
   def update
-    @post = Post.find(params[:id])
-    if @post.update(params.require(:post).permit(:title, :content))
+    if @post.update(post_params)
       redirect_to posts_path
     end
   end
 
   def destroy
-    @post = Post.find(params[:id])
     if @post.destroy
       redirect_to posts_path
     end
+  end
+
+  private
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :content)
   end
 end
