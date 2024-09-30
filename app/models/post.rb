5fd3f16b12832215fa037bec0520cfab6be82661
kodacamp <kodacamp.recruitment@gmail.com>
2024-09-30 14:36:09 +0800

    add: validation to post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,11 @@
 class Post < ApplicationRecord
+  # Todo: Read more about https://guides.rubyonrails.org/v3.2/active_record_validations_callbacks.html
+  validates :title, presence: true
+  validates :title, length: { in: 4..100 }
+  # validates :title, length: { minimum: 4, maximum: 100 }
+  validates :title, uniqueness: true
+
+  # validates :title, inclusion: { in: ['small', 'medium', 'large'] }
+
+  validates :content, presence: true, length: { minimum: 5, maximum: 4000 }
 end
