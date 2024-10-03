5a73a3d9a5c5994d5cf692f4b2a5c8e2e774a277
kodacamp <kodacamp.recruitment@gmail.com>
2024-10-03 11:35:51 +0800

    add: edit to comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,6 @@
 class CommentsController < ApplicationController
   before_action :set_post
+  before_action :set_comment, only: [:edit, :update]
 
   def index
     @comments = @post.comments
@@ -19,10 +20,25 @@ class CommentsController < ApplicationController
     end
   end
 
+  def edit; end
+
+  def update
+    if @comment.update(comment_params)
+      flash[:notice] = 'Comment updated successfully'
+      redirect_to post_comments_path(@post)
+    else
+      render :edit
+    end
+  end
+
   private
 
   def set_post
     @post = Post.find params[:post_id]
+  end
+
+  def set_comment
+    @comment = @post.comments.find(params[:id])
   end
 
   def comment_params
