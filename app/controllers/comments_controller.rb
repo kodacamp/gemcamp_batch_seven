4949ea8015ae170cf1a9792dd3f78273a1aa3c0c
kodacamp <kodacamp.recruitment@gmail.com>
2024-10-03 11:20:13 +0800

    Add: new page for comment controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,9 +5,27 @@ class CommentsController < ApplicationController
     @comments = @post.comments
   end
 
+  def new
+    @comment = @post.comments.new
+  end
+
+  def create
+    @comment = @post.comments.new(comment_params)
+    if @comment.save
+      flash[:notice] = 'Comment created successfully'
+      redirect_to post_comments_path(@post)
+    else
+      render :new
+    end
+  end
+
   private
 
   def set_post
     @post = Post.find params[:post_id]
+  end
+
+  def comment_params
+    params.require(:comment).permit(:content)
   end
 end
