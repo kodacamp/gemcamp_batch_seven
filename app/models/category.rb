4e2f840c3bd932b320516c7c874b934c39d62ed3
kodacamp <kodacamp.recruitment@gmail.com>
2024-10-04 11:42:43 +0800

    add: dependent destroy to category model

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,10 @@
 class Category < ApplicationRecord
   validates :name, presence: true
-
-  has_many :post_category_ships
+  # has_many :post_category_ships, dependent: :destroy
+  # has_many :post_category_ships, dependent: :delete_all
+  # has_many :post_category_ships, dependent: :nullify
+  # has_many :post_category_ships, dependent: :restrict_with_error
+  # has_many :post_category_ships, dependent: :restrict_with_exception
+  has_many :post_category_ships, dependent: :destroy
   has_many :posts, through: :post_category_ships
 end
