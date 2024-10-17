5c9c05ede82793d9b4d8aa317a96860d80ecd787
kodacamp <kodacamp.recruitment@gmail.com>
2024-10-17 11:45:14 +0800

    add: fetch barangays

--- app/services/ph_location_service.rb
+++ app/services/ph_location_service.rb
@@ -55,4 +55,16 @@ class PhLocationService
 
     # Todo: find 2 cities does not belongs to any province or district
   end
+
+  def fetch_barangays
+    request = RestClient.get("#{url}/barangays/")
+    barangays = JSON.parse(request.body)
+    barangays.each do |barangay|
+      city_code = barangay['cityCode'] ? barangay['cityCode'] : barangay['municipalityCode']
+      address_barangay = Address::Barangay.find_or_initialize_by(code: barangay['code'])
+      address_barangay.name = barangay['name']
+      address_barangay.city = Address::City.find_by(code: city_code)
+      address_barangay.save
+    end
+  end
 end
