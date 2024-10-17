c046aeb1640b3dc74a64a4651e85a584c0446a5c
kodacamp <kodacamp.recruitment@gmail.com>
2024-10-17 10:55:08 +0800

    add: address region model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_10_11_032045) do
+ActiveRecord::Schema[7.0].define(version: 2024_10_17_023543) do
+  create_table "address_regions", charset: "utf8mb4", force: :cascade do |t|
+    t.string "code"
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "categories", charset: "utf8mb4", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
