03e4ad9e497018b145ca3e84861057cf08d4db9f
kodacamp <kodacamp.recruitment@gmail.com>
2024-10-21 11:23:11 +0800

    add: examples for host validation

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,6 +59,9 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
+  # config.hosts << "05e6-103-43-215-138.ngrok-free.app"
+  # config.hosts.clear
+
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
 
